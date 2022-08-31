af275b8b2e2b241210e2233d233d9dcef5907573
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-31 17:13:33 -0300

    Requisito 4 - Completo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+////REQUISITO 2 E 3
 //// PALETA DE CORES
 
 let localPalet = document.getElementById("color-palette");
@@ -7,9 +8,11 @@ for (let index = 0; index < 4; index += 1) {
   divColor.style.border = "1px solid black";
   divColor.style.width = "20px";
   divColor.style.height = "20px";
+  divColor.style.margin = "5px";
   divColor.style.display = "inline-block"
   localPalet.appendChild(divColor);
 }
+
 //// PRIMEIRA COR
 let colorOne = localPalet.firstElementChild;
 colorOne.style.backgroundColor = "black";
@@ -26,3 +29,34 @@ colorThree.style.backgroundColor = "yellow";
 let colorFour = localPalet.lastElementChild;
 colorFour.style.backgroundColor = "green";
 
+//// FUNÇÃO COR ALEATÓRIA
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let color = `rgb(${r}, ${g}, ${b})`;
+  return color;
+}
+
+//// BOTÃO E CORES ALEATÓRIAS
+
+let button = document.createElement("button");
+button.id = "button-random-color";
+button.innerText = "Cores aleatórias";
+localPalet.appendChild(button);
+
+//// FUNÇÃO BOTÃO - ALEATORIEDADE DAS CORES
+
+
+function btnRandomColor() {
+  let btn = document.getElementById("button-random-color");
+  let divIndex = document.getElementsByClassName("color");
+  btn.addEventListener("click", function (colorRandom) {
+    for (index = 1; index < divIndex.length; index += 1) {
+      divIndex[index].style.backgroundColor = randomColor();
+    };
+  });
+}
+
+btnRandomColor();
